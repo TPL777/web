6bd38d51fe5eb096b0ae460649b0564aa70c50e1
EA - Thiago Pinheiro Lima <pinheiro_thiago@educadventista.org>
2023-10-30 14:54:07 -0300

    atividade

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { application } = require("express")
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+const caminhoBase = path.join(__dirname, "templates") 
+
 application.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando com o express")
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3003, () => {
