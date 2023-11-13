988029c91d0211edaf71eb83a02040e9e50e9f02
EA - Thiago Pinheiro Lima <pinheiro_thiago@educadventista.org>
2023-11-13 14:41:48 -0300

    aula 3

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates") 
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+})
+
+app.get('/cadastrar', (requisicao, resposta) => {
+    resposta.sendFile(${caminhoBase}/cadastro.html)
+})
+
 app.get('/usuarios', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/usuarios.html`)
     const id = requisicao.params.idconsole.log(`Acessando dados do usuários ${id}`)
